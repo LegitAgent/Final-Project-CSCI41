af5e5a3446be88670cc4b9a992d4efcd0bb0853c
moira-b <moiraysabelle7@gmail.com>
2025-11-23 01:48:30 +0800

    Added empty view classes

--- FinalProjectCSCI41/central_bookings/views.py
+++ FinalProjectCSCI41/central_bookings/views.py
@@ -8,3 +8,14 @@ from .models import Participant, Activity, ActivityBooking
 # Create your views here.
 
 
+class ActivityListView(ListView):
+    # show ALL activities, regardless of whether the user has enlisted in them
+    pass
+
+class ActivityDetailView(DetailView):
+    # show a particular activity and allow the user to enlist in it
+    pass
+
+class EnlistedActivityListView(ListView):
+    # show the activities which the user has enlisted in
+    pass
