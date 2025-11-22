76ffeeab9b6dd8b09e55f5798b14548d4faec470
moira-b <moiraysabelle7@gmail.com>
2025-11-23 01:49:16 +0800

    Created urls.py file. No urlpatterns yet

---
+++ FinalProjectCSCI41/central_bookings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import(
+    ActivityListView, ActivityDetailView, EnlistedActivityListView
+)
+
+urlpatterns = [
+    # PATTERNS GO HERE
+]
+
+app_name = 'central_bookings'
