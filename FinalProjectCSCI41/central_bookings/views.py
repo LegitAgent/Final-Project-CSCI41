567f2a63e0ead4a678f5af8ebdb4db59060890a0
moira-b <moiraysabelle7@gmail.com>
2025-11-23 02:04:12 +0800

    Specified each view's corresponding model and added comments

--- FinalProjectCSCI41/central_bookings/views.py
+++ FinalProjectCSCI41/central_bookings/views.py
@@ -10,12 +10,26 @@ from .models import Participant, Activity, ActivityBooking
 
 class ActivityListView(ListView):
     # show ALL activities, regardless of whether the user has enlisted in them
+
+    model = Activity
+    # template_name = # place name of html file once we have it
+    
     pass
 
 class ActivityDetailView(DetailView):
     # show a particular activity and allow the user to enlist in it
+
+    model = Activity
+    # template_name = # place name of html file once we have it
+
     pass
 
 class EnlistedActivityListView(ListView):
     # show the activities which the user has enlisted in
+    
+    model = ActivityBooking
+
+    # need to do a little bit of logic here
+    # to ensure that only the user's enlisted activities show up
+    
     pass
