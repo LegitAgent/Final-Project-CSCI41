55ce08beaa0cd082c1b5e90066ed28ac77124aa1
moira-b <moiraysabelle7@gmail.com>
2025-11-29 23:32:24 +0800

    Fixed appearance of bookings view to match activities view

--- FinalProjectCSCI41/central_bookings/views.py
+++ FinalProjectCSCI41/central_bookings/views.py
@@ -101,57 +101,61 @@ def enlist_in_activity(request, activity_id):
     
 def get_enlisted_activities_for_user(request):
     """Gets all activies that the user is enlisted in, as well as all their reservations."""
-
-    user = request.user.participant
-
-    # prevents extra database queries when you later access r.location.name
-    reservation_qs = Reservation.objects.select_related('location')
     
-    bookings_qs = (
-        ActivityBooking.objects
-        .filter(participant=user)
-        .select_related('participant', 'activity', 'activity__organizer') # avoids extra queries per booking.
-        .prefetch_related(
-            Prefetch(
-                'activity__reservations',
-                queryset=reservation_qs, to_attr='prefetched_reservations')
-            ) 
-        # prefetch reservations with locations for activities.
-        # 1st args: fetch bookings 2nd args: fetch reservations related to activities in bookings.
-        # 3rd args: store prefetched reservations in 'prefetched_reservations' attribute instead of .reservations.all().
-    )
+    try:
+        user = request.user.participant
 
-    # build a list of results (one item per reservation if multiple reservations).
-    enlisted = []
-    for booking in bookings_qs:
-        activity = booking.activity
-        organizer_name = activity.organizer.user.first_name
-        user_name = booking.participant.user.first_name
-
-        reservations = getattr(activity, 'prefetched_reservations', list(activity.reservations.all()))
-        for r in reservations:
-            # dictionary to hold details of each enlisted activity per reservation
-            enlisted.append({
-                'activity_name': activity.name,
-                'user_name': user_name,
-                'attended': booking.attended,
-                'date': r.date,
-                'start_time': r.start_time,
-                'end_time': r.end_time,
-                'location': r.location.name if r.location else None,
-                'organizer': organizer_name,
-                'activity_url': activity.get_absolute_url,
-                'activity_pk': booking.pk
-            })
+        # prevents extra database queries when you later access r.location.name
+        reservation_qs = Reservation.objects.select_related('location')
         
-        # If the user marks an activity as attended
-        # update the model isntance accordingly. 
-        if (request.method=="POST" and "attendance_check_button" in request.POST):
-            attended_booking = ActivityBooking.objects.get(pk=int(request.POST.get('activity_pk')))
-            attended_booking.mark_attended()
-            attended_booking.save()
-            return redirect(reverse('central_bookings:bookings'))
+        bookings_qs = (
+            ActivityBooking.objects
+            .filter(participant=user)
+            .select_related('participant', 'activity', 'activity__organizer') # avoids extra queries per booking.
+            .prefetch_related(
+                Prefetch(
+                    'activity__reservations',
+                    queryset=reservation_qs, to_attr='prefetched_reservations')
+                ) 
+            # prefetch reservations with locations for activities.
+            # 1st args: fetch bookings 2nd args: fetch reservations related to activities in bookings.
+            # 3rd args: store prefetched reservations in 'prefetched_reservations' attribute instead of .reservations.all().
+        )
 
-    ctx = {'enlisted_activities': enlisted, 'profile': user}
-    return render(request, 'bookings_list.html', ctx)
-    # return render(request, 'profile.html', ctx)
+        # build a list of results (one item per reservation if multiple reservations).
+        enlisted = []
+        for booking in bookings_qs:
+            activity = booking.activity
+            organizer_name = activity.organizer.user.first_name
+            user_name = booking.participant.user.first_name
+
+            reservations = getattr(activity, 'prefetched_reservations', list(activity.reservations.all()))
+            for r in reservations:
+                # dictionary to hold details of each enlisted activity per reservation
+                enlisted.append({
+                    'activity_name': activity.name,
+                    'user_name': user_name,
+                    'attended': booking.attended,
+                    'date': r.date,
+                    'start_time': r.start_time,
+                    'end_time': r.end_time,
+                    'location': r.location.name if r.location else None,
+                    'organizer': organizer_name,
+                    'activity_url': activity.get_absolute_url,
+                    'activity_pk': booking.pk
+                })
+            
+            # If the user marks an activity as attended
+            # update the model isntance accordingly. 
+            if (request.method=="POST" and "attendance_check_button" in request.POST):
+                attended_booking = ActivityBooking.objects.get(pk=int(request.POST.get('activity_pk')))
+                attended_booking.mark_attended()
+                attended_booking.save()
+                return redirect(reverse('central_bookings:bookings'))
+
+            ctx = {'enlisted_activities': enlisted, 'profile': user}
+            return render(request, 'bookings_list.html', ctx)
+    
+    except:
+        print("Non-participant user is attempting to click on bookings view.")
+        return redirect(reverse('central_bookings:activity-list'))
